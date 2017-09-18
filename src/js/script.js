41955ec800a6f5aaade3803b01c01f505f213b70
MomokoSanchez <sonia.isabel.patlan@gmail.com>
2017-09-18 10:32:52 -0600

    Navigation design and Portfolio hover

--- src/js/script.js
+++ src/js/script.js
@@ -2,9 +2,26 @@
 
 $(function() {
 
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+
+    //hover portfolio items
+    $('.portfolioImg').hover(function(){
+    	$(this).toggleClass('col-4');
+    });
+
+
+	//open up & close portfolio subpage and blur background
 	$('.portfolioImg').click( function(){
 
-		$('#portfolio'+$(this).attr('id')).removeClass('hidden');
+		$('#portfolio'+$(this).attr('id')).delay(500).fadeIn(500);
 
 		$('body').children().each(function() { 
 			$(this).addClass('blur');
@@ -12,13 +29,11 @@ $(function() {
 	});
 
 	$('.close').click( function(){
-		$('.portfolioPage').addClass('hidden');
+		$('.portfolioPage').fadeOut(400);
 
 		$('body').children().each(function() { 
 			$(this).removeClass('blur');
 		});
 	});
-
-
 
 });
