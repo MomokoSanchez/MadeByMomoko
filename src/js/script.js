47868c7ab57a45300762f55606f88f834822d9e5
MomokoSanchez <sonia.isabel.patlan@gmail.com>
2017-09-28 19:58:52 -0600

    Responsive debugging, Hero animation, new portfolio images

--- src/js/script.js
+++ src/js/script.js
@@ -1,144 +1,196 @@
 //Momoko Theme JavaScript
 
+//Sky animation for main image
+
+//update on resize
+var heroHeight =  $(window).height();
+var animationFinished = false;
+
+//change background radients
+function setBgColors($c1, $c2, $c3, $c4, $c5){
+	
+	$color1 = "rgb(" + $c1[0] + "," + $c1[1] + "," + $c1[2] + ") " + $c1[3] + "%";
+	$color2 = "rgb(" + $c2[0] + "," + $c2[1] + "," + $c2[2] + ") " + $c2[3] + "%";
+	$color3 = "rgb(" + $c3[0] + "," + $c3[1] + "," + $c3[2] + ") " + $c3[3] + "%";
+	$color4 = "rgb(" + $c4[0] + "," + $c4[1] + "," + $c4[2] + ") " + $c4[3] + "%";
+	$color5 = "rgb(" + $c5[0] + "," + $c5[1] + "," + $c5[2] + ") " + $c5[3] + "%";
+
+	$('header').css({
+		background: "rgb(" + $c1[0] + "," + $c1[1] + "," + $c1[2] + ")",
+		background: "-moz-linear-gradient(top, "+$color1+", "+$color2+", "+$color3+", "+$color4+", "+$color5+")",
+		background: "-webkit-linear-gradient(top, "+$color1+", "+$color2+", "+$color3+", "+$color4+", "+$color5+")",
+		background: "-o-linear-gradient(top, "+$color1+", "+$color2+", "+$color3+", "+$color4+", "+$color5+")",
+		background: "-ms-linear-gradient(top, "+$color1+", "+$color2+", "+$color3+", "+$color4+", "+$color5+")",
+		background: "linear-gradient(to bottom, "+$color1+", "+$color2+", "+$color3+", "+$color4+", "+$color5+")",
+		"-webkit-transition": "background .5s",
+	    "-moz-transition": "background .5s",
+	    "-ms-transition": "background .5s",
+	    "-o-transition": "background .5s",
+	    transition: "background .5s",
+	    "background-attachment": "fixed"
+	});	
+}
+
+function simulateTheSun($move) {
+
+	$mouseHeight = $move;
+
+	//day colors
+	$day1 = [25,69,130,0];
+	$day2 = [39,89,150,40];
+	$day3 = [71,129,179,75];
+	$day4 = [142,194,251,95];
+	$day5 = [193,219,251,125];
+
+	//sunrise colorls
+	$sunrise1 = [123,157,194,0];
+	$sunrise2 = [144,170,193,40];
+	$sunrise3 = [220,214,190,75];
+	$sunrise4 = [254,231,163,93];
+	$sunrise5 = [249,161,139,105];
+
+	//night colors
+	$night1 = [9,0,55,15];
+	$night2 = [47,41,79,65];
+	$night3 = [49,56,100,85];
+	$night4 = [51,78,125,95];
+	$night5 = [48,88,140,125];
+
+
+	//set night as default
+	$c1 = $night1;
+	$c2 = $night2;
+	$c3 = $night3;
+	$c4 = $night4;
+	$c5 = $night5;
+
+	$('#stars').css({
+		opacity: 1
+	})
+
+	//change colors and percentage
+	if($mouseHeight < heroHeight){
+
+		//day
+		if($mouseHeight <= (heroHeight * 0.65) ){
+			$c1[0] = Math.round( $day1[0] + ($sunrise1[0] - $day1[0]) * (  $mouseHeight / (heroHeight*0.65) ) );
+			$c1[1] = Math.round( $day1[1] + ($sunrise1[1] - $day1[1]) * (  $mouseHeight / (heroHeight*0.65) ) );
+			$c1[2] = Math.round( $day1[2] + ($sunrise1[2] - $day1[2]) * (  $mouseHeight / (heroHeight*0.65) ) );
+			$c1[3] = Math.round( $day1[3] + ($sunrise1[3] - $day1[3]) * (  $mouseHeight / (heroHeight*0.65) ) ); 
+
+			$c2[0] = Math.round( $day2[0] + ($sunrise2[0] - $day2[0]) * (  $mouseHeight / (heroHeight*0.65) ) );
+			$c2[1] = Math.round( $day2[1] + ($sunrise2[1] - $day2[1]) * (  $mouseHeight / (heroHeight*0.65) ) );
+			$c2[2] = Math.round( $day2[2] + ($sunrise2[2] - $day2[2]) * (  $mouseHeight / (heroHeight*0.65) ) );
+			$c2[3] = Math.round( $day2[3] + ($sunrise2[3] - $day2[3]) * (  $mouseHeight / (heroHeight*0.65) ) ); 
+
+			$c3[0] = Math.round( $day3[0] + ($sunrise3[0] - $day3[0]) * (  $mouseHeight / (heroHeight*0.65) ) );
+			$c3[1] = Math.round( $day3[1] + ($sunrise3[1] - $day3[1]) * (  $mouseHeight / (heroHeight*0.65) ) );
+			$c3[2] = Math.round( $day3[2] + ($sunrise3[2] - $day3[2]) * (  $mouseHeight / (heroHeight*0.65) ) );
+			$c3[3] = Math.round( $day3[3] + ($sunrise3[3] - $day3[3]) * (  $mouseHeight / (heroHeight*0.65) ) ); 
+
+			$c4[0] = Math.round( $day4[0] + ($sunrise4[0] - $day4[0]) * (  $mouseHeight / (heroHeight*0.65) ) );
+			$c4[1] = Math.round( $day4[1] + ($sunrise4[1] - $day4[1]) * (  $mouseHeight / (heroHeight*0.65) ) );
+			$c4[2] = Math.round( $day4[2] + ($sunrise4[2] - $day4[2]) * (  $mouseHeight / (heroHeight*0.65) ) );
+			$c4[3] = Math.round( $day4[3] + ($sunrise4[3] - $day4[3]) * (  $mouseHeight / (heroHeight*0.65) ) ); 
+
+			$c5[0] = Math.round( $day5[0] + ($sunrise5[0] - $day5[0]) * (  $mouseHeight / (heroHeight*0.65) ) );
+			$c5[1] = Math.round( $day5[1] + ($sunrise5[1] - $day5[1]) * (  $mouseHeight / (heroHeight*0.65) ) );
+			$c5[2] = Math.round( $day5[2] + ($sunrise5[2] - $day5[2]) * (  $mouseHeight / (heroHeight*0.65) ) );
+			$c5[3] = Math.round( $day5[3] + ($sunrise5[3] - $day5[3]) * (  $mouseHeight / (heroHeight*0.65) ) ); 
+
+			setBgColors($c1, $c2, $c3, $c4, $c5);	
+			$('#stars').css({
+				opacity: 0
+			})	
+		}
+
+		//sunrise
+		if($mouseHeight > ( heroHeight * 0.65 ) ){
+
+			setBgColors($sunrise1, $sunrise2, $sunrise3, $sunrise4, $sunrise5);
+			$('#stars').css({
+				opacity: 0
+			})
+		}
+
+		//night
+		if($mouseHeight > (heroHeight * 0.75)){
+
+			$c1[0] = Math.round( $sunrise1[0] + ( $night1[0] - $sunrise1[0]) * ( ($mouseHeight / (heroHeight * 0.25) - 3 ) ) );
+			$c1[1] = Math.round( $sunrise1[1] + ( $night1[1] - $sunrise1[1]) * ( ($mouseHeight / (heroHeight * 0.25) - 3 ) ) );
+			$c1[2] = Math.round( $sunrise1[2] + ( $night1[2] - $sunrise1[2]) * ( ($mouseHeight / (heroHeight * 0.25) - 3 ) ) );
+			$c1[3] = Math.round( $sunrise1[3] + ( $night1[3] - $sunrise1[3]) * ( ($mouseHeight / (heroHeight * 0.25) - 3 ) ) ); 
+
+			$c2[0] = Math.round( $sunrise2[0] + ( $night2[0] - $sunrise2[0]) * ( ($mouseHeight / (heroHeight * 0.25) - 3 ) ) );
+			$c2[1] = Math.round( $sunrise2[1] + ( $night2[1] - $sunrise2[1]) * ( ($mouseHeight / (heroHeight * 0.25) - 3 ) ) );
+			$c2[2] = Math.round( $sunrise2[2] + ( $night2[2] - $sunrise2[2]) * ( ($mouseHeight / (heroHeight * 0.25) - 3 ) ) );
+			$c2[3] = Math.round( $sunrise2[3] + ( $night2[3] - $sunrise2[3]) * ( ($mouseHeight / (heroHeight * 0.25) - 3 ) ) ); 
+
+			$c3[0] = Math.round( $sunrise3[0] + ( $night3[0] - $sunrise3[0]) * ( ($mouseHeight / (heroHeight * 0.25) - 3 ) ) );
+			$c3[1] = Math.round( $sunrise3[1] + ( $night3[1] - $sunrise3[1]) * ( ($mouseHeight / (heroHeight * 0.25) - 3 ) ) );
+			$c3[2] = Math.round( $sunrise3[2] + ( $night3[2] - $sunrise3[2]) * ( ($mouseHeight / (heroHeight * 0.25) - 3 ) ) );
+			$c3[3] = Math.round( $sunrise3[3] + ( $night3[3] - $sunrise3[3]) * ( ($mouseHeight / (heroHeight * 0.25) - 3 ) ) );
+
+			$c4[0] = Math.round( $sunrise4[0] + ( $night4[0] - $sunrise4[0]) * ( ($mouseHeight / (heroHeight * 0.25) - 3 ) ) );
+			$c4[1] = Math.round( $sunrise4[1] + ( $night4[1] - $sunrise4[1]) * ( ($mouseHeight / (heroHeight * 0.25) - 3 ) ) );
+			$c4[2] = Math.round( $sunrise4[2] + ( $night4[2] - $sunrise4[2]) * ( ($mouseHeight / (heroHeight * 0.25) - 3 ) ) );
+			$c4[3] = Math.round( $sunrise4[3] + ( $night4[3] - $sunrise4[3]) * ( ($mouseHeight / (heroHeight * 0.25) - 3 ) ) ); 
+
+			$c5[0] = Math.round( $sunrise5[0] + ( $night5[0] - $sunrise5[0]) * ( ($mouseHeight / (heroHeight * 0.25) - 3 ) ) );
+			$c5[1] = Math.round( $sunrise5[1] + ( $night5[1] - $sunrise5[1]) * ( ($mouseHeight / (heroHeight * 0.25) - 3 ) ) );
+			$c5[2] = Math.round( $sunrise5[2] + ( $night5[2] - $sunrise5[2]) * ( ($mouseHeight / (heroHeight * 0.25) - 3 ) ) );
+			$c5[3] = Math.round( $sunrise5[3] + ( $night5[3] - $sunrise5[3]) * ( ($mouseHeight / (heroHeight * 0.25) - 3 ) ) ); 
+
+			setBgColors($c1, $c2, $c3, $c4, $c5);	
+
+			$opacity = $mouseHeight / (heroHeight * 0.25) - 3;
+			$('#stars').css({
+				opacity: $opacity
+			})
+		}
+	}
+}
+
+$(document).ready( function(){
+	$('.innerBox').addClass('show');
+	$('.outerBox').addClass('show');
+	$('.mainText').addClass('show');
+	$('.mainText h1').delay(2500).queue(function(){
+		$(this).addClass('separator');
+	});
+
+	$counter =  $(window).height();
+	$steps = $counter / 400;
+
+	$countMe = setInterval(
+		function(){
+			if($counter >  ( $(window).height() * 0.75) ){
+				simulateTheSun($counter);
+				$counter = $counter - $steps;
+				console.log($counter);
+			}
+			else{
+				animationFinished = true;
+				clearInterval($countMe);
+			}
+		}, 50
+	);
+
+});
+
+//change Sky on mouse movement
+$(document).mousemove( function(event){
+
+	if(animationFinished){
+		simulateTheSun( event.clientY ); 
+	}
+}); 
+
 $(function() {
 
-	//Sky animation
-	//update on resize
-	var screenHeight = $(window).height();
-	var heroHeight = screenHeight * 0.7;
-
-	//change background radients
-	function setBgColors($c1, $c2, $c3, $c4, $c5){
-		
-		$color1 = "rgb(" + $c1[0] + "," + $c1[1] + "," + $c1[2] + ") " + $c1[3] + "%";
-		$color2 = "rgb(" + $c2[0] + "," + $c2[1] + "," + $c2[2] + ") " + $c2[3] + "%";
-		$color3 = "rgb(" + $c3[0] + "," + $c3[1] + "," + $c3[2] + ") " + $c3[3] + "%";
-		$color4 = "rgb(" + $c4[0] + "," + $c4[1] + "," + $c4[2] + ") " + $c4[3] + "%";
-		$color5 = "rgb(" + $c5[0] + "," + $c5[1] + "," + $c5[2] + ") " + $c5[3] + "%";
-		$('header').css({
-			background: "rgb(" + $c1[0] + "," + $c1[1] + "," + $c1[2] + ")",
-			background: "-moz-linear-gradient(top, "+$color1+", "+$color2+", "+$color3+", "+$color4+", "+$color5+")",
-			background: "-webkit-linear-gradient(top, "+$color1+", "+$color2+", "+$color3+", "+$color4+", "+$color5+")",
-			background: "-o-linear-gradient(top, "+$color1+", "+$color2+", "+$color3+", "+$color4+", "+$color5+")",
-			background: "-ms-linear-gradient(top, "+$color1+", "+$color2+", "+$color3+", "+$color4+", "+$color5+")",
-			background: "linear-gradient(to bottom, "+$color1+", "+$color2+", "+$color3+", "+$color4+", "+$color5+")",
-			"-webkit-transition": "background .5s",
-		    "-moz-transition": "background .5s",
-		    "-ms-transition": "background .5s",
-		    "-o-transition": "background .5s",
-		    transition: "background .5s",
-		    "background-attachment": "fixed"
-		});	
-	}
-
-	document.onmousemove = handleMouseMove;
-
-    function handleMouseMove(event) {
-		var mouseHeight = event.clientY;
-
-		$c1 = [123,157,194,0];
-		$c2 = [144,170,193,8];
-		$c3 = [220,214,190,49];
-		$c4 = [254,231,163,66];
-		$c5 = [249,161,139,75];
-
-		//change colors and percentage
-		if(mouseHeight < heroHeight){
-
-			//day
-			if(mouseHeight <= (heroHeight * 0.65) ){
-				$c1[0] = Math.round( 25 + (123 - 25) * (  mouseHeight / (heroHeight*0.65) ) );
-				$c1[1] = Math.round( 69 + (157 - 69) * (  mouseHeight / (heroHeight*0.65) ) );
-				$c1[2] = Math.round( 130 + (194 - 130) * (  mouseHeight / (heroHeight*0.65) ) );
-				$c1[3] =  0;
-
-				$c2[0] = Math.round( 39 + (144 - 39) * (  mouseHeight / (heroHeight*0.65) ) );
-				$c2[1] = Math.round( 89 + (170 - 89) * (  mouseHeight / (heroHeight*0.65) ) );
-				$c2[2] = Math.round( 150 + (194 - 150) * (  mouseHeight / (heroHeight*0.65) ) );
-				$c2[3] = 8 
-
-				$c3[0] = Math.round( 71 + (220 - 71) * (  mouseHeight / (heroHeight*0.65) ) );
-				$c3[1] = Math.round( 129 + (214 - 129) * (  mouseHeight / (heroHeight*0.65) ) );
-				$c3[2] = Math.round( 179 + (190 - 179) * (  mouseHeight / (heroHeight*0.65) ) );
-				$c3[3] = 49
-
-				$c4[0] = Math.round( 142 + (254 - 142) * (  mouseHeight / (heroHeight*0.65) ) );
-				$c4[1] = Math.round( 194 + (231 - 194) * (  mouseHeight / (heroHeight*0.65) ) );
-				$c4[2] = Math.round( 251 + (163 - 251) * (  mouseHeight / (heroHeight*0.65) ) );
-				$c4[3] = Math.round( 70 + (63 - 70) * (  mouseHeight / (heroHeight*0.65) ) ); 
-
-				$c5[0] = Math.round( 193 + (249 - 193) * (  mouseHeight / (heroHeight*0.65) ) );
-				$c5[1] = Math.round( 219 + (161 - 219) * (  mouseHeight / (heroHeight*0.65) ) );
-				$c5[2] = Math.round( 251 + (139 - 251) * (  mouseHeight / (heroHeight*0.65) ) );
-				$c5[3] = Math.round( 85 + (75 - 85) * (  mouseHeight / (heroHeight*0.65) ) ); 
-
-				setBgColors($c1, $c2, $c3, $c4, $c5);	
-
-				$('#stars').css({
-					opacity: 0
-				})		
-			}
-
-			//sunrise
-			if(mouseHeight > ( heroHeight * 0.65 ) ){
-				$c1 = [123,157,194,0];
-				$c2 = [144,170,193,8];
-				$c3 = [220,214,190,49];
-				$c4 = [254,231,163,63];
-				$c5 = [249,161,139,75];
-
-				setBgColors($c1, $c2, $c3, $c4, $c5);
-
-				$('#stars').css({
-					opacity: 0
-				})
-			}
-
-			//night
-			if(mouseHeight > (heroHeight * 0.75)){
-				$c1[0] = Math.round( 123 + (9 - 123) * ( (mouseHeight - (heroHeight * 0.75)) / (heroHeight * 0.25) ) );
-				$c1[1] = Math.round( 157 + (0 - 157) * ( (mouseHeight - (heroHeight * 0.75)) / (heroHeight * 0.25) ) );
-				$c1[2] = Math.round( 194 + (55 - 194) * ( (mouseHeight - (heroHeight * 0.75)) / (heroHeight * 0.25) ) );
-				$c1[3] = Math.round( 15 + (0 - 15) * (  mouseHeight / (heroHeight*0.65) ) ); 
-
-				$c2[0] = Math.round( 144 + (47 - 144) * ( (mouseHeight - (heroHeight * 0.75)) / (heroHeight * 0.25) ) );
-				$c2[1] = Math.round( 170 + (41 - 170) * ( (mouseHeight - (heroHeight * 0.75)) / (heroHeight * 0.25) ) );
-				$c2[2] = Math.round( 193 + (79 - 193) * ( (mouseHeight - (heroHeight * 0.75)) / (heroHeight * 0.25) ) );
-				$c2[3] = Math.round( 8 + (55 - 8) * ( (mouseHeight - (heroHeight * 0.75)) / (heroHeight * 0.25) ) ); 
-
-				$c3[0] = Math.round( 220 + (49 - 220) * ( (mouseHeight - (heroHeight * 0.75)) / (heroHeight * 0.25) ) );
-				$c3[1] = Math.round( 214 + (56 - 214) * ( (mouseHeight - (heroHeight * 0.75)) / (heroHeight * 0.25) ) );
-				$c3[2] = Math.round( 190 + (100 - 190) * ( (mouseHeight - (heroHeight * 0.75)) / (heroHeight * 0.25) ) );
-				$c3[3] = Math.round( 49 + (65 - 49) * ( (mouseHeight - (heroHeight * 0.75)) / (heroHeight * 0.25) ) );
-
-				$c4[0] = Math.round( 254 + (51 - 254) * ( (mouseHeight - (heroHeight * 0.75)) / (heroHeight * 0.25) ) );
-				$c4[1] = Math.round( 231 + (78 - 231) * ( (mouseHeight - (heroHeight * 0.75)) / (heroHeight * 0.25) ) );
-				$c4[2] = Math.round( 163 + (125 - 163) * ( (mouseHeight - (heroHeight * 0.75)) / (heroHeight * 0.25) ) );
-				$c4[3] = Math.round( 66 + (70 - 66) * ( (mouseHeight - (heroHeight * 0.75)) / (heroHeight * 0.25) ) ); 
-
-				$c5[0] = Math.round( 249 + (48 - 249) * ( (mouseHeight - (heroHeight * 0.75)) / (heroHeight * 0.25) ) );
-				$c5[1] = Math.round( 161 + (88 - 161) * ( (mouseHeight - (heroHeight * 0.75)) / (heroHeight * 0.25) ) );
-				$c5[2] = Math.round( 139 + (140 - 139) * ( (mouseHeight - (heroHeight * 0.75)) / (heroHeight * 0.25) ) );
-				$c5[3] = 75;
-
-				setBgColors($c1, $c2, $c3, $c4, $c5);	
-
-				$opacity = (mouseHeight - (heroHeight * 0.75)) / (heroHeight * 0.25);
-				$('#stars').css({
-					opacity: $opacity
-				})
-			}
-		}
-    }
-
-	$c1 = [123,157,194,0];
-	$c2 = [144,170,193,8];
-	$c3 = [220,214,190,49];
-	$c4 = [254,231,163,66];
-	$c5 = [249,161,139,75];
-
-	setBgColors($c1, $c2, $c3, $c4, $c5);
-
+	//fade text in on scroll
+	window.sr = ScrollReveal();
+	sr.reveal('.reveal', { origin: 'bottom', duration: 1000, delay: 500, });
 
     // jQuery for page scrolling feature - requires jQuery Easing plugin
     $('a.page-scroll').bind('click', function(event) {
@@ -159,7 +211,7 @@ $(function() {
 
 	//open portfolio subpage and blur background
 	$('.portfolioImg').click( function(){
-		$('#portfolio'+$(this).attr('id')).delay(500).fadeIn(500);
+		$('#portfolio'+$(this).attr('id')).delay(300).fadeIn(700);
 		$('body').addClass('blur');
 	});
 
@@ -173,6 +225,7 @@ $(function() {
 	$('.close').click( function(){
 		$('.portfolioPage').fadeOut(400);
 		$('body').removeClass('blur');
+		$('.multipleImg').removeClass('itemImageSmall itemImageLarge');
 	});
 
 	//hover multiple portfolio detail pages
@@ -183,9 +236,11 @@ $(function() {
 		$('.leftImg').addClass('itemImageSmall');
 	})
 	$('.leftImg').hover(function(){
-		$(this).addClass('itemImageLarge');
-		$('.rightImg').removeClass('itemImageLarge');
-		$(this).removeClass('itemImageSmall');
-		$('.rightImg').addClass('itemImageSmall');
-	})
+		if($(this).hasClass('itemImageSmall')){
+			$(this).addClass('itemImageLarge');
+			$('.rightImg').removeClass('itemImageLarge');
+			$(this).removeClass('itemImageSmall');
+			$('.rightImg').addClass('itemImageSmall');		
+		}
+	})  
 });
